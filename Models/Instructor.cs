1747d49b9589b59bbf02e1a9b34d722a995712c8
admin <admin@707-17>
2026-04-01 11:30:35 +0700

    Add project files.

---
+++ Models/Instructor.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolAppCoreMVC.Models
+{
+    public class Instructor
+    {
+        public int InstructorID { get; set; }
+        public required string LastName { get; set; }
+        public required string FirstName { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+}
